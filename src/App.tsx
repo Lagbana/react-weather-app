c1f3cf5305e283bef4f7b738e2a84545ab2ccbac
Larry Agbana <larryagbana@gmail.com>
2021-01-11 22:46:34 -0500

    autocomplete component and effects

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,42 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo, useRef, memo } from "react";
 import "./App.css";
 import { Button, List, ListItem, Card, Flex, Grid } from "component-library-ld";
-import { ApiCaller } from "./helpers/ApiCaller";
-import { ApiResponse } from './types'
+import { ApiCaller, UserLocation } from "./helpers";
+import { getApiKey } from "./utils/getApiKey";
+import { ApiResponse } from "./types";
 import GoogleAutocomplete from "react-google-places-autocomplete";
 
 
-const App = () => {
+const App = memo(() => {
+  const [locationObj, setLocationObj] = useState<any>(undefined);
+  const count = useRef(0)
+  let renders = count.current++
+
+
+  useEffect(() => {
+    UserLocation().then((res) => {
+      // if (renders <= 2) {
+      console.log(`🟩🟩🟩`, count.current);
+      ApiCaller(res);
+      // }
+    });
+  }, []);
+
+
+
+
+
+
+
+
+
+
 
   const namesList: string[] = ["Ottawa", "Toronto", "Chicago"];
-
+  const { googleApiKey: apiKey } = getApiKey();
   const listItems = namesList.map((item, index) => {
     return (
-      <ListItem key={index.toString()} onClick={() => { }}>
+      <ListItem key={index.toString()} onClick={() => {}}>
         {item}
       </ListItem>
     );
@@ -21,10 +45,27 @@ const App = () => {
     <div className="App">
       <header className="App-header">James Fan</header>
       <List flexDirection={"column"}>{listItems}</List>
-      <GoogleAutocomplete />
-      <Button onClick={async() => { await ApiCaller("Ottawa")}}>{"Get weather response from API"}</Button>
+      <GoogleAutocomplete
+        apiKey={apiKey}
+        autocompletionRequest={{
+          componentRestrictions: { country: ["us", "ca"] },
+        }}
+        minLengthAutocomplete={3}
+        onLoadFailed={(error) => alert(`Error: ${error}`)}
+        selectProps={{
+          locationObj,
+          onChange: setLocationObj,
+        }}
+      />
+      <Button
+        onClick={async () => {
+          await ApiCaller("Ottawa, Canada");
+        }}
+      >
+        {"Get weather response from API"}
+      </Button>
     </div>
   );
-};
+})
 
 export default App;
